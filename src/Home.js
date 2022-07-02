ab92754da61de5f6afdd62d4513dcedc0c8aa941
Daniel Markow <daniel.markow@outlook.com>
2022-07-02 16:35:10 +0200

    modified image css

--- src/Home.js
+++ src/Home.js
@@ -10,14 +10,12 @@ export default function Home() {
       <Row>
         <Col>
           <h2>Greetings</h2>
-        </Col>
-        <Col>
-          <img src="portrait.png" alt="portrait of me" />
-        </Col>
-      </Row>
-      <Row>
           <p>Welcome to my portfolio page. I am Daniel, 35 years old from Brunswick in Germany. I am a Programmer, Data Engineer, Analytics Developer, AWS person, IT project manager and aspiring full stack developer.</p>
           <p>In my free time I like to spend time with my beautiful girlfriend, exercise, take fotos and travel.</p>
+        </Col>
+        <Col>
+          <img src="danielmarkow/portrait.png" alt="portrait of me eating soup" id="portrait"/>
+        </Col>
       </Row>
     </Container>
   );
