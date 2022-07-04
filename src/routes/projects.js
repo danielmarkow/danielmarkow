41ed064556f87148da7f3f130b2d5e4fd1ded5ae
Daniel Markow <daniel.markow@outlook.com>
2022-07-04 19:15:08 +0200

    added search to projects page

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,20 +1,62 @@
 import * as React from 'react';
 
 import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 import Row from 'react-bootstrap/row';
+
 import Project from './project';
 import myProjects from './myProjects';
 
-export default function Projects() {
+function Projects() {
+    const [searchField, setSearchField] = React.useState("");
+
+    const filteredProjects = myProjects.filter(
+        project => {
+          return (
+            project
+            .name
+            .toLowerCase()
+            .includes(searchField.toLowerCase()) ||
+            project
+            .description
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+          );
+        }
+      );
+
+    function handleChange(event) {
+        setSearchField(event.target.value);
+    }
 
     return (
         <Container fluid="sm">
             <Row>
                 <h2>Projects</h2>
+                <InputGroup className="mb-3" style={{width: '30vw', alignItems: 'center', display: 'flex'}}>
+                    <FormControl 
+                        placeholder='project name or description'
+                        aria-label='project name or description'
+                        onChange = {handleChange}
+                    />
+                </InputGroup>
             </Row>
             <Row xs={1} md={2} className="g-4">
-                {myProjects.map((myProject) => <div key={myProject.id}><Project project={myProject} /></div>)}
+                {filteredProjects.map((myProject) => <div key={myProject.id}><Project project={myProject} /></div>)}
             </Row>
         </Container>
     );
 }
+
+export default Projects;
+
+/*
+*/
+
+/*
+<input 
+                    type = "search" 
+                    placeholder = "Search People" 
+                    onChange = {handleChange}
+                />*/
