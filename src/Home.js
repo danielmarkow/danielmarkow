20c8b12cb4c9b99f28a4e2161dc5b81db0267ccd
Daniel Markow <daniel.markow@outlook.com>
2022-07-05 18:37:10 +0200

    styling the video for mobile

--- src/Home.js
+++ src/Home.js
@@ -8,16 +8,17 @@ export default function Home() {
   return (
     <Container fluid="sm">
       <Row>
-        <Col>
+    
           <h2>Greetings</h2>
           <p>Welcome to my portfolio page. I am Daniel, 35 years old from Brunswick in Germany. I am a Programmer, Data Engineer, Analytics Developer, AWS person, IT project manager and aspiring full stack developer.</p>
           <p>In my free time I like to spend time with my beautiful girlfriend, exercise, take fotos and travel.</p>
-        </Col>
-        <Col>
-          <iframe className="embedded-video" width="auto" height="315" src="https://www.youtube.com/embed/ICR1jEzxgk0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </Col>
+    
      
       </Row>
+        <div className="embedded-video">
+          <iframe src="https://www.youtube.com/embed/ICR1jEzxgk0" frameborder="0" allowfullscreen></iframe>
+        </div>
+    
     </Container>
   );
 }
