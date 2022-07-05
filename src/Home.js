86fa6b05b767972b7622ec065fd830290dcf6b18
Daniel Markow <daniel.markow@outlook.com>
2022-07-05 18:11:21 +0200

    embedded video style finetuning

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
           <p>In my free time I like to spend time with my beautiful girlfriend, exercise, take fotos and travel.</p>
         </Col>
         <Col>
-          <iframe className="embedded-video" width="560" height="315" src="https://www.youtube.com/embed/ICR1jEzxgk0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe className="embedded-video" width="auto" height="315" src="https://www.youtube.com/embed/ICR1jEzxgk0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Col>
      
       </Row>
